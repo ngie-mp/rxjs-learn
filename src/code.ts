80fb4b3711a95e61c1c3f2001c5a5711aab0618f
Angie <angeles.morap@gmail.com>
2019-06-26 09:32:25 +0200

    Add an addItem function to observable
    Display it in markup

--- src/code.ts
+++ src/code.ts
@@ -5,4 +5,11 @@ var observable = Observable.create((observer: any) => {
 });
 
 
-observable.subscribe((x:any) => console.log(x));
+observable.subscribe((x:any) => addItem(x));
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
