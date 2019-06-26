a9226dc45895cc3afa62dfc7b21f2bfc17a26635
Angie <angeles.morap@gmail.com>
2019-06-26 16:31:40 +0200

    Use 'skipUntil' operator

--- src/code.ts
+++ src/code.ts
@@ -1,14 +1,24 @@
-import { from } from 'rxjs/observable/from';
-import 'rxjs/add/operator/pluck';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { interval } from 'rxjs/observable/interval';
+import 'rxjs/add/operator/skipUntil';
 
-from([
-    {first: 'Mary', last: 'Simon', age: 65 },
-    {first: 'Jane', last: 'The virgin', age: 25 },
-    {first: 'Marc', last: 'Antony', age: 42 }
-])
+var observable1 = Observable.create((data:any) => {
+    var i = 1
+    setInterval(() => {
+        data.next(i++)
+    },1000)
+})
 
-    .pluck('age')
-    .subscribe((x:any) => addItem(x))
+var observable2 = new Subject; 
+
+setTimeout(() => {
+    observable2.next('Hey!')
+}, 5000)
+
+var newObs = observable1.skipUntil(observable2)
+
+newObs.subscribe((x:any) => addItem(x))
 
 function addItem(val:any) {
     var node = document.createElement("li");
