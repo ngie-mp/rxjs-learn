6b256431e9e22c5e7b4a1f4c648a56283238dfd2
Angie <angeles.morap@gmail.com>
2019-06-26 16:06:28 +0200

    User 'map' operator
    > It passes each source value through a transformation function to
    get corresponding output values.

--- src/code.ts
+++ src/code.ts
@@ -1,17 +1,11 @@
 import { Observable } from 'rxjs/Observable';
-import { merge } from 'rxjs/observable/merge';
+import 'rxjs/add/operator/map';
 
-var observable = Observable.create((observer:any) => {
+Observable.create((observer:any) => {
     observer.next('Hey guys!')
 }) 
-
-var observable2 = Observable.create((observer:any) => {
-    observer.next('Whats up?')
-}) 
-
-var newObs = merge(observable, observable2)
-
-newObs.subscribe((x:any) => addItem(x))
+    .map((val:any) => val.toUpperCase())
+    .subscribe((x:any) => addItem(x))
 
 function addItem(val:any) {
     var node = document.createElement("li");
